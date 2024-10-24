2a7a2429cd691c2908fd77ac3b1223a90593723b
Magnus HK <magnushoholt@pm.me>
2024-10-24 22:00:26 +0200

    player can add a symbol

--- main.ts
+++ main.ts
@@ -1,10 +1,19 @@
 const  arrayRows: number = 3;
 const  arrayCols: number = 3;
 
-let tictacArray: string[][] = Array.from({ length: arrayRows}, () => Array(arrayCols).fill(" "));
+let board: string[][] = Array.from({ length: arrayRows}, () => Array(arrayCols).fill(" "));
+
+export function setPlayer(): string {
+    let player: string | null;
+    do {
+        player = prompt("Select X or O? By typing");
+    }while(!player || player !== "X" && player !== "O");
+
+    return player;
+}
 
 export function printArray(){
-    tictacArray.forEach(function(row){
+    board.forEach(function(row){
         console.log(row)
     });
 }
@@ -22,11 +31,29 @@ export function playerMove(): [number, number] | null {
             console.log("Invalid input!")
         }
     }
-
     return null
+}
+
+function makeMove(player: string): void {
+    let move: [number,number] | null;
+    do {
+        move = playerMove();
+    }while(!move || !isValidMove(board, move));
+
+    const [row,col] = move;
+
+    board[row][col] = player;
+
+    printArray();
+}
+
+function isValidMove(board: string[][], move: [number,number]): boolean{
+    const [row,col] = move;
+    return board[row][col] === " ";
 }
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main){
     printArray();
+    makeMove(setPlayer());
 }
