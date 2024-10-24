6d3a68a027f7b6516e1b10b98830d6448ea69971
Magnus Høholt Kaspersen <magnushoholt@pm.me>
2024-10-24 08:35:53 +0200

    added player input

--- main.ts
+++ main.ts
@@ -1,8 +1,32 @@
-export function add(a: number, b: number): number {
-  return a + b;
+const  arrayRows: number = 3;
+const  arrayCols: number = 3;
+
+let tictacArray: string[][] = Array.from({ length: arrayRows}, () => Array(arrayCols).fill(" "));
+
+export function printArray(){
+    tictacArray.forEach(function(row){
+        console.log(row)
+    });
+}
+
+export function playerMove(): [number, number] | null {
+    const input = prompt("Enter a row and column number separated by a space, to play")
+    
+    if (input) {
+        const [row, col] = input.split(" ").map(Number);
+
+        if(row >= 0 && row <= 2 && col >= 0 && col <= 2){
+            return [row, col];
+        }
+        else {
+            console.log("Invalid input!")
+        }
+    }
+
+    return null
 }
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+if (import.meta.main){
+    printArray();
 }
